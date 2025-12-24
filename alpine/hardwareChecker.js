8437eaee2915b82f6e7268a2d99eb704118f6f94
ruby <hyoka4654@gmail.com>
2025-12-24 15:41:53 +0800

    test

--- alpine/hardwareChecker.js
+++ alpine/hardwareChecker.js
@@ -1,5 +1,8 @@
 const { ipcRenderer } = require('electron');
 const si = require('systeminformation');
+const { exec } = require('child_process');
+const util = require('util');
+const execPromise = util.promisify(exec);
 
 const hardwareChecker = () => ({
   progress: 0,
@@ -16,6 +19,20 @@ const hardwareChecker = () => ({
     available: 0,
     usedPercent: 0,
     display: ''
+  },
+
+  async getNvidiaDriver() {
+    try {
+      const { stdout } = await execPromise('nvidia-smi --query-gpu=driver_version --format=csv,noheader');
+      const version = stdout.trim();
+      if (version) {
+        return version
+      }
+      return ''
+    } catch (e) {
+      console.log('nvidia-smi not found, trying alternative method');
+      return ''
+    }
   },
 
   async checkHardware() {
@@ -37,9 +54,8 @@ const hardwareChecker = () => ({
     
     // gpu
     const gpuData = await si.graphics();
-    this.driverVersion = gpuData.controllers.length == 2 ? gpuData.controllers[1].driverVersion : 'Unknown';
-    console.log('GPU Data:', this.driverVersion);
-    this.graphics = gpuData.controllers.map(gpu => `${gpu.model} (${gpu.vram / 1024} MB)`);
+    this.driverVersion = await this.getNvidiaDriver();
+    this.graphics = gpuData.controllers.map(gpu => gpu.model);
     this.progress = 50;
 
     // memory
@@ -63,33 +79,6 @@ const hardwareChecker = () => ({
         display: `C槽：${total}GB 可用(${available}GB)`
       };
     }
-
-    // si.getAllData().then(data => {
-      // this.os = `${data.os.distro} ${data.os.arch} ${data.os.release}`;
-      
-      // // CPU
-      // this.cpu = data.cpu.brand;
-      // this.cpuCores = data.cpu.cores;
-      
-      // // RAM (記憶體) - 從 data.mem 取得
-      // this.memory = Math.round(data.mem.total / 1024 / 1024 / 1024); // 轉換成 GB
-      // console.log('RAM:', this.memory + ' GB');
-      
-      // // 顯示卡
-      // this.graphics = data.graphics.controllers;
-      
-      // // C 槽資訊
-      // const cDrive = data.fsSize.find(disk => disk.mount === 'C:');
-      // if (cDrive) {
-      //   this.diskC = {
-      //     total: Math.round(cDrive.size / 1024 / 1024 / 1024), // GB
-      //     used: Math.round(cDrive.used / 1024 / 1024 / 1024), // GB
-      //     available: Math.round((cDrive.size - cDrive.used) / 1024 / 1024 / 1024), // GB
-      //     usedPercent: Math.round(cDrive.use)
-      //   };
-      // }
-      
-    //});
 
     this.status = 'finished';
   },
